6767e2b97e5f4f57fe153f72482cef8b7c64d9b5
Davide <dalzilio.davide@gmail.com>
2023-10-03 11:52:28 +0200

    First Commit Add base project

---
+++ src/CloseSidesTabs/Commands/CloseTabsToLeft.cs
@@ -0,0 +1,10 @@
+﻿namespace CloseSidesTabs.Commands;
+
+[Command(PackageIds.CloseTabsToLeft)]
+internal sealed class CloseTabsToLeft : BaseCommand<CloseTabsToLeft>
+{
+    protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+    {
+        await VS.MessageBox.ShowWarningAsync("CloseTabsToLeft", "Button clicked");
+    }
+}
---
+++ src/CloseSidesTabs/Commands/CloseTabsToRight.cs
@@ -0,0 +1,10 @@
+﻿namespace CloseSidesTabs.Commands;
+
+[Command(PackageIds.CloseTabsToRight)]
+internal sealed class CloseTabsToRight : BaseCommand<CloseTabsToRight>
+{
+    protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+    {
+        await VS.MessageBox.ShowWarningAsync("CloseSidesTabs", "Button clicked");
+    }
+}
